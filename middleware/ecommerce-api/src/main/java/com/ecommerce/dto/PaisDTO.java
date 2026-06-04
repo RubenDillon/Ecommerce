16ca31f5ad5a3a025563a36b7f3955e481ca93f3
Ruben Dillon <26331064+RubenDillon@users.noreply.github.com>
2026-06-04 15:10:37 -0300

    Update PaisDTO.java

--- middleware/ecommerce-api/src/main/java/com/ecommerce/dto/PaisDTO.java
+++ middleware/ecommerce-api/src/main/java/com/ecommerce/dto/PaisDTO.java
@@ -1,33 +1,13 @@
-// src/main/java/com/ecommerce/dto/PaisDTO.java
 package com.ecommerce.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaisDTO {
     private Long idPais;
     private String nombre;
-
-    // Constructors
-    public PaisDTO() {
-    }
-
-    public PaisDTO(Long idPais, String nombre) {
-        this.idPais = idPais;
-        this.nombre = nombre;
-    }
-
-    // Getters and Setters
-    public Long getIdPais() {
-        return idPais;
-    }
-
-    public void setIdPais(Long idPais) {
-        this.idPais = idPais;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 }
