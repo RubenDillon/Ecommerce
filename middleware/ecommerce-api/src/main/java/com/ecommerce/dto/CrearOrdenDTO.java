017590f32d237342b70126af077e5bc98436f6e3
Ruben Dillon <26331064+RubenDillon@users.noreply.github.com>
2026-06-04 11:55:54 -0300

    Create CrearOrdenDTO.java

---
+++ middleware/ecommerce-api/src/main/java/com/ecommerce/dto/CrearOrdenDTO.java
@@ -0,0 +1,23 @@
+package com.ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CrearOrdenDTO {
+    private Long idCliente;
+    private List<ItemOrdenDTO> items;
+    
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ItemOrdenDTO {
+        private Long idProducto;
+        private Integer cantidad;
+    }
+}
