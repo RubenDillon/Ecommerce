f46953f696e179ddffac139e55d4ab198602e4d2
Ruben Dillon <26331064+RubenDillon@users.noreply.github.com>
2026-06-04 13:06:55 -0300

    Create CiudadResource.java

---
+++ middleware/ecommerce-api/src/main/java/com/ecommerce/resource/CiudadResource.java
@@ -0,0 +1,56 @@
+// src/main/java/com/ecommerce/resource/CiudadResource.java
+package com.ecommerce.resource;
+
+import com.ecommerce.dto.CiudadDTO;
+import com.ecommerce.service.CiudadService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.List;
+
+@Path("/api/ciudades")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class CiudadResource {
+
+    @Inject
+    CiudadService ciudadService;
+
+    @GET
+    public List<CiudadDTO> getAll() {
+        return ciudadService.findAll();
+    }
+
+    @GET
+    @Path("/{id}")
+    public CiudadDTO getById(@PathParam("id") Long id) {
+        return ciudadService.findById(id);
+    }
+
+    @GET
+    @Path("/pais/{idPais}")
+    public List<CiudadDTO> getByPais(@PathParam("idPais") Long idPais) {
+        return ciudadService.findByPais(idPais);
+    }
+
+    @POST
+    public Response create(CiudadDTO ciudadDTO) {
+        CiudadDTO created = ciudadService.create(ciudadDTO);
+        return Response.status(Response.Status.CREATED).entity(created).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public CiudadDTO update(@PathParam("id") Long id, CiudadDTO ciudadDTO) {
+        return ciudadService.update(id, ciudadDTO);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response delete(@PathParam("id") Long id) {
+        ciudadService.delete(id);
+        return Response.noContent().build();
+    }
+}
