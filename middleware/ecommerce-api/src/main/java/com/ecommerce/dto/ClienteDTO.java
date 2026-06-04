202597988c42cbb34b2ad7628d983cfa8cbd5c5e
Ruben Dillon <26331064+RubenDillon@users.noreply.github.com>
2026-06-04 14:30:59 -0300

    Update ClienteDTO.java

--- middleware/ecommerce-api/src/main/java/com/ecommerce/dto/ClienteDTO.java
+++ middleware/ecommerce-api/src/main/java/com/ecommerce/dto/ClienteDTO.java
@@ -1,23 +1,91 @@
+// src/main/java/com/ecommerce/dto/ClienteDTO.java
 package com.ecommerce.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ClienteDTO {
     private Long idCliente;
     private String nombre;
     private String direccion;
     private Long idCiudad;
-    private String nombreCiudad;
     private Long idPais;
-    private String nombrePais;
     private String telefono;
-    private String email;
-    private LocalDateTime fechaRegistro;
+    private String correoElectronico;
+    private CiudadDTO ciudad;
+    private PaisDTO pais;
+
+    // Constructors
+    public ClienteDTO() {
+    }
+
+    // Getters and Setters
+    public Long getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Long idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Long getIdCiudad() {
+        return idCiudad;
+    }
+
+    public void setIdCiudad(Long idCiudad) {
+        this.idCiudad = idCiudad;
+    }
+
+    public Long getIdPais() {
+        return idPais;
+    }
+
+    public void setIdPais(Long idPais) {
+        this.idPais = idPais;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public CiudadDTO getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(CiudadDTO ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public PaisDTO getPais() {
+        return pais;
+    }
+
+    public void setPais(PaisDTO pais) {
+        this.pais = pais;
+    }
 }
